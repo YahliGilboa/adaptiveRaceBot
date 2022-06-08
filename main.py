b778d4533e016dfd51a436c461ee0559eedb978e
Yahli Gilboa <yahli.gilboa@gmail.com>
2022-06-08 12:14:48 +0300

    removed rockets :(

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ racerMaxVelocity = 6
 carLimit = 6
 
 
-def checkCollisions(racer,Cars,rocket):
+def checkCollisions(racer,Cars):
     global drawColCar,drawColRaceCar
     for car in Cars:
         if racer.isCollided(car) and racer.immunity == 0:
@@ -57,31 +57,9 @@ def checkCollisions(racer,Cars,rocket):
             racer.decHealth()
             racer.immunity = immunityTime
             #racer- rocket collision:
-    if rocket[0] != None and racer.immunity == 0:
-        if racer.isCollided(rocket[0]):
-            spawnExplosion(rocket)
-            rocket[0] = None
-            racer.decHealth()
-            racer.immunity = immunityTime
-        else:
-            for car in Cars:
-                if rocket[0].isCollided(car):
-                    spawnExplosion(rocket)
-                    rocket[0] = None
-                    Cars.remove(car)
-                    break
-    if rocket[1] != 0 and racer.immunity == 0:
-        if racer.isCollided(rocket[1]):
-            racer.decHealth()
-            racer.immunity = immunityTime
 
-def spawnExplosion(rocket):
-    if rocket[0].side == 0:
-        rocket[1] = Explosion([rocket[0].position[0]+50,rocket[0].position[1]-33],explosionCollider,[0,0],explosionMaxTime,explosionIMG)
-        rocket[0] = None
-    else:
-        rocket[1] = Explosion([rocket[0].position[0] + rocket[0].texture.get_width()- 50 - explosionIMG.get_width(), rocket[0].position[1] - 33], explosionCollider, [0, 0],explosionMaxTime, explosionIMG)
-        rocket[0] = None
+
+
 def spawnCar(lane,color,velocity,Cars):
     #if 4 lanes are occupied, and this cars lane is the one left do not spawn unless the velocity is greater or smaller to the one of the average of all cars velocity (preventing line of cars technique)
 
@@ -153,52 +131,12 @@ def randomiserCar(Cars):#will handle return a car object, if no car spawns retur
         velocity =  (random.randint(3, 8))
         spawnCar(lane,color,velocity,Cars)
 
-def handleRocket(rocket,racer):
-    global rocketMinTime, rocketMaxTime, rocketTimer, rocketTargetTime
-    #print(rocketTimer)
-    if rocket[0] == None:
-        if rocketTargetTime == 0:
-            rocketTargetTime = random.randint(rocketMinTime,rocketMaxTime)
-        if rocketTimer == rocketTargetTime:
-            side = random.randint(0,1) #side of rocket spawn, 0 is left 1 is right
-            rndmAddRocket = (random.randint(0,200)*(racer.velocity[1]/6))
-            if side == 0:
-                rocket[0] = Rocket([-rocketIMG.get_width(),racer.position[1]+83+rndmAddRocket], rocketCollider, [5, 0], side, rocketIMG) #spawns rocket "around" middle of player (83 from plyer position) the player y location and depending ton which way he is going, spawns at speed that wil seem like screen is moving forward, "screen speed" is 2
 
-
-            else:
-                rocket[0] = Rocket([rocketIMG.get_width()+screenWidth, racer.position[1] + 83+rndmAddRocket], rocketCollider[::-1], [-5, 0], side, pygame.transform.rotate(rocketIMG, 180))#rotate image,flip collision box by slicing
-
-            rocketTimer = 0
-            rocketTargetTime = 0
-
-        else:
-            rocketTimer += 1
-    elif rocket[0].velocity[0] < 0:
-        if rocket[0].velocity[0] > -15:
-            rocket[0].velocity[0] -= 0.2
-    else:
-        if rocket[0].velocity[0] < 15:
-            rocket[0].velocity[0] += 0.2
-
-def handleDespawn(Cars,rocket): #will handle despawn of all objects (cars, rockets, etc)
+def handleDespawn(Cars): #will handle despawn of all objects (cars, rockets, etc)
     for car in Cars:
         if car.position[1] >= screenHeight:
             Cars.remove(car)
-    if rocket[0] != None:
-        # if rocket[0].position[1] == screenHeight:
-        #     rocket[0] = None
-        if rocket[0].side == 0:
-            if rocket[0].position[0] >= screenWidth:
-                rocket[0] = None
-        else:
-            if rocket[0].position[0] <= 0 - rocket[0].texture.get_width():
-                rocket[0] = None
-    if rocket[1] != 0:
-        if rocket[1].deSpawnTimer == 0:
-            rocket[1] = 0
-        else:
-            rocket[1].deSpawnTimer -= 1
+
 
 def draw_collider(entity):
     for collisionBox in entity.collider:
@@ -266,7 +204,7 @@ def indexToMove(index,racer):
     racer.changePos()
 
 
-def draw_win(background,racers,Cars,rocket):
+def draw_win(background,racers,Cars):
     global drawColCar,drawColRaceCar
     #print(drawColCar,drawColRaceCar)
     WIN.blit(background,(0,0))
@@ -284,14 +222,11 @@ def draw_win(background,racers,Cars,rocket):
     for car in Cars:
         WIN.blit(car.texture,tuple(car.position))
         #handleDrawnHealth(racer)
-        if rocket[0] != None:
-            WIN.blit(rocket[0].texture, tuple(rocket[0].position))
-        if rocket[1] != 0:
-            WIN.blit(rocket[1].texture,tuple(rocket[1].position))
+
     pygame.display.update() #actually updates all changes made to screen
 
 
-def retInputs(Cars, racer, rocket):
+def retInputs(Cars, racer):
         normliser = 100000 #this value is used so that the tanh function will not just give aproximations of -1 and 1 to values (good for games like flappy bird and internet dinosaur, not for my game)
         racerLatitude = racer.position[1] + 75  # latitude line, around the middle of the car
         racerXpos = racer.position[0] + 31 #gives the x coordinate of middle of racer
@@ -299,7 +234,7 @@ def retInputs(Cars, racer, rocket):
         lanes = [[], [], [], [], []]
         for car in Cars:
             lanes[car.lane].append(car)
-        output = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,racerXpos/normliser]
+        output = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,racerXpos/normliser]
         index = 0
         for lane in lanes:
             for car in lane:
@@ -318,10 +253,6 @@ def retInputs(Cars, racer, rocket):
 
             index += 2
 
-        if rocket[0] != None:
-            output[10] = ((racer.position[0] + 28) - rocket[0].position[0])/normliser
-            output[11] = (racerLatitude - rocket[0].position[1])/normliser
-        return output
 
 def main(genomes,config):
     nets = []
@@ -340,7 +271,6 @@ def main(genomes,config):
     #     print(racer.position)
     #     print("\n")
     Cars =[]
-    rocket = [None,0] #if rocket is 0 then there is no rocket on field. if it is a rocket type object than this object will act accordingly on field, second cell is for explosion. has to be list so i can pass by reference
     #clock = pygame.time.Clock()
     stop = False
 
@@ -355,15 +285,13 @@ def main(genomes,config):
         #handlePlayerMovement(racer,keysPressed)
         for car in Cars:
             car.changePos()
-        if rocket[0] != None:
-            rocket[0].changePos()
+
         for racer in racers:
-            checkCollisions(racer,Cars,rocket)
-            handleRocket(rocket, racer)
+            checkCollisions(racer,Cars)
 
         randomiserCar(Cars)
 
-        handleDespawn(Cars, rocket)
+        handleDespawn(Cars)
 
 
         #kills all losing racers
@@ -383,12 +311,11 @@ def main(genomes,config):
         #         ge[x].fitness += 0.5
 
 
-
         for x, racer in enumerate(racers):
             # print(racer)
             # print(racer.position)
             ge[x].fitness += 1
-            outputs = nets[x].activate(tuple(retInputs(Cars,racer,rocket)))
+            outputs = nets[x].activate(tuple(retInputs(Cars,racer)))
 
                 # this decides what the player will do
                 #find max output, meaning what car wants to go to:
@@ -415,7 +342,7 @@ def main(genomes,config):
             if racer.immunity != 0:
                 racer.immunity -= 1
 
-        draw_win(background,racers,Cars,rocket)
+        draw_win(background,racers,Cars)
         score += 1
         if score > 1000:
             break
@@ -435,7 +362,7 @@ def run(config_path):
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
 
-    winner = p.run(main,10000)
+    winner = p.run(main,1000)
     pickle_out = open("theBot","wb")
     pickle.dump(winner,pickle_out)
     pickle_out.close()
